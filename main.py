10b90d0de84e7ef847e75e2be2de275c33854e24
Marseille <r3ven.cool@gmail.com>
2020-10-15 21:01:33 +0300

    123

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import matplotlib as plt
 import cv2
+import numpy
 
 
 def train(x, y):
@@ -13,26 +14,37 @@ def train(x, y):
     trained_model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
     trained_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    trained_model.fit(x, y, epochs=10)
+    trained_model.fit(x, y, epochs=3)
     trained_model.save('model')
     return trained_model
 
 
-mnist = tf.keras.datasets.mnist
+def load_data():
+    mnist = tf.keras.datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-img = cv2.imread("mydata/1.png")
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_norm = img_gray/255
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+    x_train = tf.keras.utils.normalize(x_train, axis=1)
+    x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+    x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+    x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+    return (x_train, y_train), (x_test, y_test)
+
+
+# (x_train, y_train), (x_test, y_test) = load_data()
+
+img = cv2.cvtColor(cv2.imread("mydata/2.png"), cv2.COLOR_BGR2GRAY) / 255
+valid_image = numpy.empty(shape=(28, 28, 1))
+
+for i in range(0, len(img)):
+    for j in range(0, len(img[i])):
+        valid_image[i][j] = numpy.array(img[i][j])
+
+my_data = numpy.array([valid_image])
 
 # model = train(x_train, y_train)
 model = tf.keras.models.load_model('model')
+result = model.predict(my_data)
 # val_loss, val_acc = model.evaluate(x_test, y_test)
-
-print(model.predict(img_norm.reshape(img_norm.shape[0], 28, 28, 1)))
+print(numpy.argmax(result))
