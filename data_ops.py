04b985d565d02e73f70617982e42e9e2b13936c4
Marseille <r3ven.cool@gmail.com>
2020-12-13 20:35:30 +0300

    done

--- data_ops.py
+++ data_ops.py
@@ -10,28 +10,23 @@ if __name__ == '__main__':
 
 def load_my_data():
     data_folder = str(Path().absolute()) + "\\mydata"
-    files = []
-    map = dict({})
-    for (dirpath, dirnames, filenames) in os.walk(data_folder):
-        for dirname in dirnames
-        for filename in filenames:
-            files.append("mydata/" + filename)
+    categories = dict({})
 
-    images = []
+    for folder in os.listdir(data_folder):
+        images = []
+        for file in os.listdir(data_folder + "/" + folder):
+            img = cv2.cvtColor(cv2.imread(data_folder + '/' + folder + '/' + file), cv2.COLOR_BGR2GRAY) / 255
+            valid_image = numpy.empty(shape=(28, 28, 1))
+            for i in range(0, len(img)):
+                for j in range(0, len(img[i])):
+                    valid_image[i][j] = numpy.array(img[i][j])
+            images.append(valid_image)
 
-    for file in files:
-        img = cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2GRAY) / 255
-        valid_image = numpy.empty(shape=(28, 28, 1))
-        for i in range(0, len(img)):
-            for j in range(0, len(img[i])):
-                valid_image[i][j] = numpy.array(img[i][j])
-        images.append(valid_image)
-
-    my_data = numpy.array(images)
-    return my_data
+        categories[folder] = numpy.array(images)
+    return categories
 
 
-def load_minst_data():
+def load_mnist_data():
     mnist = tf.keras.datasets.mnist
 
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-#import numpy
+import numpy
 import data_ops
-#import model_ops
+import model_ops
 
-
-# (x_train, y_train), (x_test, y_test) = data_ops.load_minst_data()
+# (x_train, y_train), (x_test, y_test) = data_ops.load_mnist_data()
 
 my_data = data_ops.load_my_data()
-#model = model_ops.load()
-#results = model.predict(my_data)
+model = model_ops.load()
 
-#for result in results:
-    #print(numpy.argmax(result))
+for key in my_data.keys():
+    results = model.predict(my_data[key])
+
+    correct = 0
+    for result in results:
+        if key == str(numpy.argmax(result)):
+            correct += 1
+    print('for', key, 'valid', correct, '/', len(results), ';', 'acc:', float(correct) / len(results))
