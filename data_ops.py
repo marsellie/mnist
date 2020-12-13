5bbbd7c9a663351194767b0d20fdb3a867ad8b19
Marseille <r3ven.cool@gmail.com>
2020-12-13 20:55:15 +0300

    done

--- data_ops.py
+++ data_ops.py
@@ -10,20 +10,19 @@ if __name__ == '__main__':
 
 def load_my_data():
     data_folder = str(Path().absolute()) + "\\mydata"
-    categories = dict({})
-
+    y = []
+    x = []
     for folder in os.listdir(data_folder):
-        images = []
         for file in os.listdir(data_folder + "/" + folder):
             img = cv2.cvtColor(cv2.imread(data_folder + '/' + folder + '/' + file), cv2.COLOR_BGR2GRAY) / 255
             valid_image = numpy.empty(shape=(28, 28, 1))
             for i in range(0, len(img)):
                 for j in range(0, len(img[i])):
                     valid_image[i][j] = numpy.array(img[i][j])
-            images.append(valid_image)
+            y.append(int(folder))
+            x.append(valid_image)
 
-        categories[folder] = numpy.array(images)
-    return categories
+    return numpy.array(x), numpy.array(y)
 
 
 def load_mnist_data():
--- main.py
+++ main.py
@@ -2,16 +2,9 @@ import numpy
 import data_ops
 import model_ops
 
-# (x_train, y_train), (x_test, y_test) = data_ops.load_mnist_data()
 
-my_data = data_ops.load_my_data()
+(x_train, y_train), (x_test, y_test) = data_ops.load_mnist_data()
+
 model = model_ops.load()
-
-for key in my_data.keys():
-    results = model.predict(my_data[key])
-
-    correct = 0
-    for result in results:
-        if key == str(numpy.argmax(result)):
-            correct += 1
-    print('for', key, 'valid', correct, '/', len(results), ';', 'acc:', float(correct) / len(results))
+x_my_test, y_my_test = data_ops.load_my_data()
+model.evaluate(x_my_test, y_my_test)
